4ba90e3490938c069c4892d3aea5518b0b8cdf8c
Kim1090 <pitsamai.classic@gmail.com>
2018-01-12 17:40:44 +0700

    add route delete video and images

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const http = require('http');
-const router = require('routes')();
+var app = require('express')();
+var bodyParser = require('body-parser');
 const Busboy = require('busboy');
 const inspect = require('util').inspect;
 const fs = require('fs');
@@ -13,15 +13,21 @@ const config = require('./config');
 const previewImage = (file, filename) => {
   const previewSmall = `${config.imagePath}/small_${filename}.jpg`;
   const previewBig = `${config.imagePath}/big_${filename}.jpg`;
-  // /usr/local/bin/ffmpeg
-  exec(`/usr/bin/ffmpeg -loglevel panic -y -i "${file}" -frames 1 -q:v 1 -vf fps=1,scale=535x346 ${previewBig} -frames 1 -q:v 1 -vf fps=1,scale=200x130 ${previewSmall}`,function(error, stdout, stderr){
+  // ffmpeg local path : /usr/local/bin/ffmpeg
+  // ffmpeg server : usr/bin/ffmpeg
+  exec(`/usr/local/bin/ffmpeg -loglevel panic -y -i "${file}" -frames 1 -q:v 1 -vf fps=1,scale=535x346 ${previewBig} -frames 1 -q:v 1 -vf fps=1,scale=200x130 ${previewSmall}`,function(error, stdout, stderr){
     if (error) {
       console.log('--------err cut preview image----------', error);
     };
   });
 };
 
-router.addRoute('/upload-video', function (req, res, params) {
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.post('/upload-video', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -69,12 +75,37 @@ router.addRoute('/upload-video', function (req, res, params) {
     res.end('some thing wrong, please check you data');
   }
 });
- 
-const server = http.createServer(function (req, res) {
-  const match = router.match(req.url);
-  if (match) match.fn(req, res, match.params);
+
+app.get('/delete-video', function (req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  if(req.query.fileId) {
+    const imageSmall = `${config.imagePath}/small_${req.query.fileId}.jpg`;
+    const imageBig = `${config.imagePath}/big_${req.query.fileId}.jpg`;
+    const video = `${config.vodPath}/${req.query.fileId}.mp4`
+
+    console.log('---imageSmall--->>', imageSmall);
+    console.log('---imageBig--->>', imageBig);
+
+    exec(`rm -rf ${imageSmall} ${imageBig} ${video}`,function(error, stdout, stderr) {
+      if (error) {
+        console.log('--------err delete images and video----------', error);
+      };
+    });
+    res.writeHead(200, { 'Connection': 'close' });
+    res.end();
+  } else {
+    res.writeHead(403, { 'Connection': 'close' });
+    res.end('Pleas check file Id');
+  }
 });
- 
-server.listen(port, function () {
-  console.log('start on port ' + port);
+
+
+app.listen(port, function () {
+  console.log('Starting node.js on port ' + port);
 });
+
+module.exports = app;
